7de2d47852d454526aefbc39e0aa763fd69247e1
Jacsc21 <93648426+Jacsc21@users.noreply.github.com>
2023-05-29 13:29:29 +0200

    Big changes

--- fdl.hs
+++ fdl.hs
@@ -10,10 +10,10 @@ data Rule = Rule Char State deriving Show
 data Command = Forward | LeftTurn Int | RightTurn Int | Nop | Scale Double | Backward deriving Show
 
 -- turtle represented by reference to window, current position, current angle, current length
-type Turtle = (Window, Double, Double, Double, Double)
+type Turtle = (Window, Double, Double, Double, Double, RGB, Float)
 
 -- fractal represented by initial state, list of rules, mapping from letters to commands, target depth, initial length
-type Fractal = (State, [Rule], Char -> Command, Int, Double)
+type Fractal = (State, [Rule], Char -> Command, Int, Double, RGB, Float)
 
 -- examples for rule lists
 rules1 = [Rule 'F' "FLFRFLF"]
@@ -66,19 +66,19 @@ processHelper = map
 
 -- retrieves the start state of a given fractal
 getState :: Fractal -> State
-getState (state, _, _, _, _) = state
+getState (state, _, _, _, _, _, _) = state
 
 -- retrieves the rules of a given fractal
 getRule :: Fractal -> [Rule]
-getRule (_, rules, _, _, _) = rules
+getRule (_, rules, _, _, _, _, _) = rules
 
 -- retrieves the target depth of a given fractal
 getDepth :: Fractal -> Int
-getDepth (_, _, _, depth, _) = depth
+getDepth (_, _, _, depth, _, _, _) = depth
 
 -- retreives the mapping of a given fractal
 getMapping :: Fractal -> Char -> Command
-getMapping (_, _, mapping, _, _) = mapping
+getMapping (_, _, mapping, _, _, _, _) = mapping
 
 -- helper function to go from two floating point values to a pair of integers
 toPoint :: Double -> Double -> Point
@@ -88,29 +88,32 @@ toPoint x y = (round x, round y)
 drawIt :: Turtle -> [Command] -> IO ()
 drawIt _                     []                    = return ()
 drawIt turtle                (Nop:xs)              = drawIt turtle xs
-drawIt (w, x, y, angle, len) (Forward:xs)          = let
+drawIt (w, x, y, angle, len, rgb, width) (Forward:xs)          = let
   x' = x+len*(cos angle)
   y' = y-len*(sin angle)
+  rgb = RGB 0 0 255
+  widthInt = round 5.0
   in do
-    drawInWindow w (line (toPoint x y) (toPoint x' y'))
-    drawIt (w, x', y', angle, len) xs
-drawIt (w, x, y, angle, len) (Backward:xs)          = let
+    p <- createPen Solid widthInt rgb
+    drawInWindow w (withPen p (line (toPoint x y) (toPoint x' y')))
+    drawIt (w, x', y', angle, len, rgb, width) xs
+drawIt (w, x, y, angle, len, rgb, width) (Backward:xs)          = let
   x' = x-len*(cos angle)
   y' = y+len*(sin angle)
   in do
     drawInWindow w (line (toPoint x y) (toPoint x' y'))
-    drawIt (w, x', y', angle, len) xs
-drawIt (w, x, y, angle, len) (LeftTurn degree:xs)  = let angle' = angle+(fromIntegral degree)*pi/180 in
-    drawIt (w, x, y, angle', len) xs
-drawIt (w, x, y, angle, len) (RightTurn degree:xs) = let angle' = angle-(fromIntegral degree)*pi/180 in
-    drawIt (w, x, y, angle', len) xs
-drawIt (w, x, y, angle, len) (Scale factor:xs)     = let len' = len*factor in
-    drawIt (w, x, y, angle, len') xs
+    drawIt (w, x', y', angle, len, rgb, width) xs
+drawIt (w, x, y, angle, len, rgb, width) (LeftTurn degree:xs)  = let angle' = angle+(fromIntegral degree)*pi/180 in
+    drawIt (w, x, y, angle', len, rgb, width) xs
+drawIt (w, x, y, angle, len, rgb, width) (RightTurn degree:xs) = let angle' = angle-(fromIntegral degree)*pi/180 in
+    drawIt (w, x, y, angle', len, rgb, width) xs
+drawIt (w, x, y, angle, len, rgb, width) (Scale factor:xs)     = let len' = len*factor in
+    drawIt (w, x, y, angle, len', rgb, width) xs
 
 -- draw a fractal by opening a window and using turtlegraphics
-drawFractal fractal@(_, _, _, _, len) = runGraphics (withWindow_ "L-System using Turtle Graphics" (1000, 600)
+drawFractal fractal@(_, _, _, _, len, rgb, width) = runGraphics (withWindow_ "L-System using Turtle Graphics" (1000, 600)
         (\ w -> do
-         drawIt (w, 500, 300, 0, len) (process fractal)
+         drawIt (w, 500, 300, 0, len, rgb, width) (process fractal)
          getKey w))
 
 -- create a list of lists of string, where each list contains the "words" of one line
@@ -124,20 +127,23 @@ split (' ':xs)  ys zss = split xs [] (reverse ys:zss)
 split (x:xs)    ys zss = split xs (x : ys) zss
 
 -- interpret the list of lists as a description of initial state, rules, mapping, depth, and length of a fractal
-interpret :: [[String]] -> State -> [Rule] -> (Char -> Command) -> Int -> Double -> Fractal
-interpret []                   start rules m depth len = (start, rules, m, depth, len)
-interpret (("rule":xs):yss)    start rules m depth len = interpret yss start (rules ++ [interpretRule xs]) m depth len where
+interpret :: [[String]] -> State -> [Rule] -> (Char -> Command) -> Int -> Double -> RBG -> Float -> Fractal
+interpret []                   start rules m depth len rgb width = (start, rules, m, depth, len, rgb, width)
+interpret (("rule":xs):yss)    start rules m depth len rgb width = interpret yss start (rules ++ [interpretRule xs]) m depth len rgb width where
   interpretRule (l:"->":rs) = Rule (head l) (foldr (++) [] rs)
-interpret (("start":xs):yss)   _     rules m depth len = interpret yss (foldr (++) [] xs) rules m depth len
-interpret (("cmd":xs):yss)     start rules m depth len = interpret yss start rules (interpretCmd xs m) depth len where
+interpret (("start":xs):yss)   _     rules m depth len rgb width = interpret yss (foldr (++) [] xs) rules m depth len rgb width 
+interpret (("cmd":xs):yss)     start rules m depth len rgb width = interpret yss start rules (interpretCmd xs m) depth len rgb width where
   interpretCmd [c, "fd"]            m = \x -> if x == (head c) then Forward else m x
   interpretCmd [c, "lt", deg]       m = \x -> if x == (head c) then LeftTurn (read deg) else m x
   interpretCmd [c, "rt", deg]       m = \x -> if x == (head c) then RightTurn (read deg) else m x
   interpretCmd [c, "nop"]           m = \x -> if x == (head c) then Nop else m x
   interpretCmd [c, "scale", factor] m = \x -> if x == (head c) then Scale (read factor) else m x
   interpretCmd [c, "bk"]            m = \x -> if x == (head c) then Backward else m x
-interpret (("length":arg:xs):yss) start rules m depth _   = interpret yss start rules m depth (read arg)
-interpret (("depth":arg:xs):yss)  start rules m _     len = interpret yss start rules m (read arg) len
+interpret (("length":arg:xs):yss) start rules m depth _ rgb width   = interpret yss start rules m depth (read arg) rgb width 
+interpret (("depth":arg:xs):yss)  start rules m _     len rgb width = interpret yss start rules m (read arg) len rgb width
+interpret (("color":arg:xs):yss)  start rules m depth len _   width = interpret yss start rules m depth len (read arg) width
+interpret (("width":arg:xs):yss)  start rules m depth len rgb _     = interpret yss start rules m depth len rgb (read arg)
+
 
 -- read from given file and return a fractal
 readFractal fileName = do
